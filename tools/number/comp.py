1eee32476db116e8847e45d7c91e62583da94855
Mohameddaif123 <154259489+Mohameddaif123@users.noreply.github.com>
2024-04-20 13:54:49 +0300

    Update comp.py

--- tools/number/comp.py
+++ tools/number/comp.py
@@ -1,9 +1,14 @@
-
 ## sum of digits function 
 def sumofdigits():
-    num = int(input(" this is  sum of digits func - Enter a number: "))
+    # Prompt the user to input a number for which the sum of digits will be calculated
+    num = int(input("This is the sum of digits function - Enter a number: "))
+    # Convert the number to a string and iterate over each digit, converting it back to an integer and summing them up
     return sum(int(digit) for digit in str(num))
-## palindrom function
+
+## palindrome function
 def ispal():
-    num = int(input("this is  oalindrom func - Enter a number: "))
+    # Prompt the user to input a number to check if it's a palindrome
+    num = int(input("This is the palindrome function - Enter a number: "))
+    # Convert the number to a string and check if it's equal to its reverse
     return str(num) == str(num)[::-1]
+s
