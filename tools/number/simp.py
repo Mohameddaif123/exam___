ae5668f89a5e4c3605808915a79fedc82738c542
Mohameddaif123 <154259489+Mohameddaif123@users.noreply.github.com>
2024-04-20 13:55:05 +0300

    Update simp.py

--- tools/number/simp.py
+++ tools/number/simp.py
@@ -1,10 +1,17 @@
 ## add function
 def add():
+    # Prompt the user to input the first number for the addition operation
     x = int(input("Enter the first number in the add operation: "))
+    # Prompt the user to input the second number for the addition operation
     y = int(input("Enter the second number in the add operation: "))
+    # Return the sum of the two numbers
     return x + y
-## sybtract function
+
+## subtract function
 def subtract():
+    # Prompt the user to input the first number for the subtraction operation
     x = int(input("Enter the first number in the sub operation: "))
+    # Prompt the user to input the second number for the subtraction operation
     y = int(input("Enter the second number in the sub operation: "))
+    # Return the result of subtracting the second number from the first number
     return x - y
